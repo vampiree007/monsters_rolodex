f9485927f8b7dc1fe71763438b2db72dc328f877
vikrant gupta <vikrantgupta.psit@gmail.com>
2020-03-21 16:39:31 +0530

    Updates

---
+++ precache-manifest.0d10d218d2049375642b929776637e23.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "062feb1895e1761d644a05114c836db5",
+    "url": "/monsters_rolodex/index.html"
+  },
+  {
+    "revision": "d6cbc85e27d1b99c317c",
+    "url": "/monsters_rolodex/static/css/main.2bac466d.chunk.css"
+  },
+  {
+    "revision": "59f2fdba054039f46ad8",
+    "url": "/monsters_rolodex/static/js/2.4f42ef37.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters_rolodex/static/js/2.4f42ef37.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d6cbc85e27d1b99c317c",
+    "url": "/monsters_rolodex/static/js/main.b10a3d95.chunk.js"
+  },
+  {
+    "revision": "16adb5c4f699e531cc3b",
+    "url": "/monsters_rolodex/static/js/runtime-main.c9fe0d04.js"
+  }
+]);
