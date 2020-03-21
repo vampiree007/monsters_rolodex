640b2d246edd937b14bdef53a3bea11f144f5b63
vikrant gupta <vikrantgupta.psit@gmail.com>
2020-03-21 16:32:46 +0530

    Updates

---
+++ precache-manifest.c24d235b6d12c2376afd058714a759a2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b7a8eb7b7b65537aabee06aa81c9fb50",
+    "url": "/monters_rolodex/index.html"
+  },
+  {
+    "revision": "3e6372b42ab5baec52f1",
+    "url": "/monters_rolodex/static/css/main.2bac466d.chunk.css"
+  },
+  {
+    "revision": "59f2fdba054039f46ad8",
+    "url": "/monters_rolodex/static/js/2.4f42ef37.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monters_rolodex/static/js/2.4f42ef37.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3e6372b42ab5baec52f1",
+    "url": "/monters_rolodex/static/js/main.ca423934.chunk.js"
+  },
+  {
+    "revision": "75371732abe543a7b300",
+    "url": "/monters_rolodex/static/js/runtime-main.b7d55bf2.js"
+  }
+]);
